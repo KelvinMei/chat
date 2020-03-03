63dc2f7b1717be56acd2f610884156d74676f107
unknown <facekelvin@live.com>
2020-03-03 00:27:07 -0700

    some parts

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var connections = [];
+var users = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on("connection", function(socket) {
+  connections.push(socket.id);
+  console.log("number of connections: " + connections.length);
+  console.log(connections);
+
+  //disconnect
+  socket.on("disconnect", function(data) {
+    //user
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+
+    //connection
+    connections.splice(connections.indexOf(socket.id), 1);
+    console.log("number of users/connections: " + connections.length);
+  });
+
+  //send message
+  socket.on("send message", function(msg) {
     const date = new Date();
     var hr = date.getHours();
     var min = date.getMinutes();
     if (min < 10) {
-        min = "0" + min;
+      min = "0" + min;
     }
     var time = hr + ":" + min;
 
-    io.emit('chat message', time + " " + msg);
+    msg = time + " " + socket.username + ": " + msg;
+
+    io.emit("new message", msg);
   });
+
+  //new user
+  socket.on("new users", function(name) {
+    socket.username = name;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    console.log(users);
+    io.emit("get users", users);
+  }
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
